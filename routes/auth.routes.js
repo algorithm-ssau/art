2543446fa0b71164ff387d5b711cc698714e63e9
Anastasia Lovtsova <lovcova.anastasi@mail.ru>
2021-03-30 02:00:43 +0400

    base register/login

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,4 +1,20 @@
 const {Router} = require('express')
+const User = require('../models/User')
 const router = Router()
+
+// /api/auth/register
+router.post('/register', async (req, res) => {
+    try {
+        const { email, password} = req.body
+
+    } catch (e) {
+        res.status(500).json({ message: 'Что-то пошло не так, попробуйте ещё раз.' })
+    }
+})
+
+// /api/auth/login
+router.post('/login', async (req, res) => {
+
+})
 
 module.exports = router
