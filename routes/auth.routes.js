77f213dc629d2cd294b14c43a1c52521e6f7be7b
Anastasia Lovtsova <lovcova.anastasi@mail.ru>
2021-03-30 02:47:51 +0400

    user register

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,4 +1,5 @@
 const {Router} = require('express')
+const bcrypt = require('bcryptjs')
 const User = require('../models/User')
 const router = Router()
 
@@ -6,6 +7,16 @@ const router = Router()
 router.post('/register', async (req, res) => {
     try {
         const { email, password} = req.body
+
+        const candidate = await User.findOne({ email: email})
+        if (candidate) {
+            return res.status(400).json({ message: 'Пользователь с таким именем уже существует.' })
+        }
+        const hashedPassword = await bcrypt.hash(password, 12)
+
+        const user = new User({ email, password: hashedPassword })
+        await user.save()
+        res.status(201).json({ message: 'Пользователь успешно создан.' })
 
     } catch (e) {
         res.status(500).json({ message: 'Что-то пошло не так, попробуйте ещё раз.' })
