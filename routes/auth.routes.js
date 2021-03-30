16d262fecfa7af6f885fb5d6a673c9141a95bf1e
Артем <gilmizakirov.artem@gmail.ru>
2021-03-30 15:43:27 +0400

    i want love

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,5 +1,7 @@
 const {Router} = require('express')
 const bcrypt = require('bcryptjs')
+const config = require('config')
+const jwt = require('jsonwebtoken')
 const {check, validationResult} = require('express-validator')
 const User = require('../models/User')
 const router = Router()
@@ -41,8 +43,48 @@ router.post(
 })
 
 // /api/auth/login
-router.post('/login', async (req, res) => {
+router.post('/login',
+    [
+        check('email', 'Введите корректный email').normalizeEmail().isEmail(),
+        check('password', 'Введите пароль').exists()
+    ],
+    async (req, res) => {
+        try {
+            const errors = validationResult(req)
 
+            if (!errors.isEmpty()){
+                return res.status(400).json({
+                    errors:errors.array(),
+                    message: 'Некорректные данные при входе в систему'
+                })
+            }
+
+            const {email, pasword} = req.body
+
+            const user = await User.findOne({email})
+
+            if(!user){
+                return res.status(400).json({message: 'Пользователь не найден'})
+            }
+
+            const isMatch = await bcrypt.compare(pasword, user.password)
+
+            if (!isMatch) {
+                return res.status(400).json({message: 'Неверный пароль, попробуйте снова'})
+            }
+
+            const token = jwt.sign(
+                { userId: user.id },
+                        config.get('jwtSecret'),
+                {expiresIn: '1h'}
+            )
+
+            res.json({token, userId: user.id})
+
+
+        } catch (e) {
+            res.status(500).json({ message: 'Что-то пошло не так, попробуйте ещё раз.' })
+        }
 })
 
 module.exports = router
