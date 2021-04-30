5c2e6ff122f98937814d7504dddd329f01d2bdf6
couldwhite <75570282+couldwhite@users.noreply.github.com>
2021-04-30 23:19:42 +0400

    Update user

--- models/User.js
+++ models/User.js
@@ -3,7 +3,9 @@ const {Schema, model, Types} = require('mongoose')
 const schema = new Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    links: [{ type: Types.ObjectId, ref: 'Link' }]
+    links: [{ type: Types.ObjectId, ref: 'Link' }],
+    //Это надо удалить
+    jokes: [{type: Types.DO, ref: 'Cookies'}]
 })
 
 module.exports = model('User', schema)
