9c374e60678b90a003ce9176855ee2bfefbe94a5
couldwhite <75570282+couldwhite@users.noreply.github.com>
2021-05-19 00:51:50 +0400

    Add python server

---
+++ python_backend/api.py
@@ -0,0 +1,34 @@
+from flask import json
+from flask_pymongo import PyMongo
+import flask
+
+app = flask.Flask(__name__)
+app.config[
+    "MONGO_URI"] = "mongodb+srv://user:user@cluster0.w25yn.mongodb.net/travel?retryWrites=true&w=majority"
+mongo = PyMongo(app)
+
+
+@app.route("/", methods=['GET'])
+def home():
+    return "Server PY"
+
+
+@app.route("/countries", methods=['GET'])
+def get_countries():
+    countries = mongo.db.countries.find()
+    data = []
+    for country in countries:
+        item = {
+            "name": country["name"],
+            "description": country["description"],
+            "imageUrl": country["imageUrl"],
+            "bigPhoto": country["bigPhoto"],
+            "information": country["information"]
+        }
+        data.append(item)
+    return json.dumps(data, ensure_ascii=False).encode('utf8')
+
+@app.route("/reviews", methods=['GET'])
+
+if __name__ == '__main__':
+    app.run(port=2000)
